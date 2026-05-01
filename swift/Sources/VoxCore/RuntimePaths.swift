b81099e09c084517c6e58bc8819f7167317916ef
Arach <arach@tchoupani.com>
2026-04-30 21:16:30 -0400

    🔖 Bump version to 0.2.2

--- swift/Sources/VoxCore/RuntimePaths.swift
+++ swift/Sources/VoxCore/RuntimePaths.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public enum VoxVersion {
-    public static let current = "0.2.0"
+    public static let current = "0.2.2"
 }
 
 public struct VoxPortDefinition: Sendable, Equatable {
