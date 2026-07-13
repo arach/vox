f3f1394f4828954a619d8dd285b87dbbfe2ae15c
Arach Tchoupani <arach@tchoupani.com>
2026-07-12 23:48:31 -0400

    🔖 Release Vox 0.3.5
    Patch release so npm publishes package READMEs for @voxd/sdk,
    @voxd/client, and @voxd/cli.

--- swift/Sources/VoxCore/RuntimePaths.swift
+++ swift/Sources/VoxCore/RuntimePaths.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public enum VoxVersion {
-    public static let current = "0.3.4"
+    public static let current = "0.3.5"
 }
 
 public struct VoxPortDefinition: Sendable, Equatable {
